351e6c4383ee7273186e75cf6f722752af87411f
Dixy-05 <ddbarahona89@gmail.com>
2021-06-08 07:58:20 -0600

    two numbers passing

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 export function sum(text) {
   if (text == '') {
     return 0;
-  }else{
+  } else {
+    if (text.indexOf(',') == -1) {
       return parseInt(text);
+    }
+    let number1 = parseInt(text.split(',')[0]);
+    let number2 = parseInt(text.split(',')[1]);
+
+    return number1 + number2;
   }
 }
