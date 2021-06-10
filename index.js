18f0f1f596ecf5dee54803a6d3d0642d34d177f1
Dixy-05 <ddbarahona89@gmail.com>
2021-06-10 11:46:24 -0600

    find new line and replace with comma

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ export function sum(text) {
     if (text.indexOf(',') == -1) {
       return parseInt(text);
     }
-    let numbers = text.split(',').map(num=>parseInt(num));
-    
-    let sum = numbers.reduce((acum, val)=>{
-      return acum += parseInt(val);
+    let match = text.match('\n');
+    let replace = text.replace(match, ',');
+    let numbers = replace.split(',').map((num) => parseInt(num));
+    let sum = numbers.reduce((acum, val) => {
+      return (acum += parseInt(val));
     });
 
     return sum;
